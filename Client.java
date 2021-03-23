ffb1a7035b8f6d3f1b61ca09a03ac03e2bffd39b
Shamil Shukurov <shamill.shukurov@gmail.com>
2021-03-23 22:51:57 +0400

    changes in Client.java

--- Client.java
+++ Client.java
@@ -4,28 +4,27 @@ import java.util.Stack;
 
 
 public class Client {
-	
+	private static Calculator calculator;
+	private static Stack<String> history = new Stack<String>();
 	public static void main ( String args []) {
-		Stack<String> history = new Stack<String>();
+		
 	try {
-		Calculator obj = new ServerConnection("localhost", 1099).connect();
+		calculator = new ServerConnection("localhost", 1099).connect();
 		System.out.println("Input expression you want to calculate");
 		Scanner input = new Scanner(System.in);
 		while(true) {
 			System.out.print(">");
 			String expression = input.nextLine();
 			if(expression.equalsIgnoreCase("history")) {
-				System.out.println(getHistory(history));
+				System.out.println(getHistory());
 				continue;
 			}
 			if(expression.equalsIgnoreCase("exit")) {
 				break;
 			}
-			String res = obj.calculate(expression);
+			String res = calculator.calculate(expression);
 			// Add successful calculation to the history
-			if(res.contains("=")) {
-				history.push(res);
-			}
+			addHistory(res);
 			System.out.println(res);
 			
 		}
@@ -34,13 +33,22 @@ public class Client {
 		System.out.println (" Client exception : " +e);
 		}
 	}
-	private static String getHistory(Stack<String> history) {
+	private static String getHistory() {
 		if(history.isEmpty()) {
 			return "Empty history";
 		}
 		else {
 			return history.pop();
 		}
+	}
+	
+	private static void addHistory(String expression) {
+		if(expression.contains("=")) {
+			history.push(expression.split("=")[0]);
+		}
+	}
+	public static Calculator getCalculator() {
+		return calculator;
 	}
 	
 }
